a99c66cb8b31cc7882704c620455778b88e1425b
Tanapl02 <92655043+Tanapl02@users.noreply.github.com>
2021-11-13 15:20:44 +0700

    Initialize dependabot/npm_and_yarn/jest-puppeteer-6.0.0

--- components/Search.tsx
+++ components/Search.tsx
@@ -14,10 +14,9 @@ import styles from './Search.module.scss'
 type SearchResult = {
   url: string
   breadcrumbs: string
+  heading: string
   title: string
   content: string
-  score: number
-  popularity: number
 }
 
 type Props = {
@@ -56,10 +55,6 @@ export function Search({
       /* await */ fetchSearchResults((router.query.query as string).trim())
     }
   }, [])
-
-  useEffect(() => {
-    closeSearch()
-  }, [currentVersion, language])
 
   // Search with your keyboard
   useEffect(() => {
@@ -196,7 +191,6 @@ export function Search({
           activeHit={activeHit}
           setActiveHit={setActiveHit}
           onGotoResult={onGotoResult}
-          debug={'debug' in router.query}
         />
       </div>
       {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
@@ -266,7 +260,6 @@ function ShowSearchResults({
   activeHit,
   setActiveHit,
   onGotoResult,
-  debug,
 }: {
   isOverlay: boolean
   isLoading: boolean
@@ -274,7 +267,6 @@ function ShowSearchResults({
   activeHit: number
   setActiveHit: (index: number) => void
   onGotoResult: (url: string, index: number) => void
-  debug: boolean
 }) {
   const { t } = useTranslation('search')
 
@@ -298,7 +290,7 @@ function ShowSearchResults({
     // When there are search results, it doesn't matter if this is overlay or not.
     return (
       <ol data-testid="search-results" className="d-block mt-4">
-        {results.map(({ url, breadcrumbs, title, content, score, popularity }, index) => {
+        {results.map(({ url, breadcrumbs, heading, title, content }, index) => {
           const isActive = index === activeHit
           return (
             <li
@@ -324,27 +316,17 @@ function ShowSearchResults({
                     className={'d-block opacity-60 text-small pb-1'}
                     dangerouslySetInnerHTML={{ __html: breadcrumbs }}
                   />
-                  {debug && (
-                    <small className="float-right">
-                      score: {score.toFixed(4)} popularity: {popularity.toFixed(4)}
-                    </small>
-                  )}
                   <div
                     className={cx(styles.searchResultTitle, 'd-block f4 text-semibold')}
                     dangerouslySetInnerHTML={{
-                      __html: title,
+                      __html: heading ? `${title}: ${heading}` : title,
                     }}
                   />
-                  {content ? (
-                    <div
-                      className={cx(styles.searchResultContent, 'd-block overflow-hidden')}
-                      dangerouslySetInnerHTML={{ __html: content }}
-                    />
-                  ) : (
-                    <div className={cx(styles.searchResultContent, 'd-block overflow-hidden')}>
-                      <i>{t('no_content')}</i>
-                    </div>
-                  )}
+                  <div
+                    className={cx(styles.searchResultContent, 'd-block overflow-hidden')}
+                    style={{ maxHeight: '4rem' }}
+                    dangerouslySetInnerHTML={{ __html: content }}
+                  />
                 </a>
               </div>
             </li>
